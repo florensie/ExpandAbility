b4877550760adc79b13479ac271153f9ab08ca09
Florens Pauwels <mail@florens.be>
2021-03-31 00:26:12 +0200

    Log spam testing

--- common/src/main/java/be/florens/swimmies/Swimmies.java
+++ common/src/main/java/be/florens/swimmies/Swimmies.java
@@ -1,8 +1,12 @@
 package be.florens.swimmies;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
 public class Swimmies {
 
 	public static final String MOD_ID = "swimmies";
+	public static final Logger LOGGER = LogManager.getLogger(MOD_ID);
 
 	public static void init() {
 		System.out.println("WAB BAP");
--- common/src/main/java/be/florens/swimmies/mixin/common/EntityMixin.java
+++ common/src/main/java/be/florens/swimmies/mixin/common/EntityMixin.java
@@ -1,12 +1,30 @@
 package be.florens.swimmies.mixin.common;
 
+import be.florens.swimmies.Swimmies;
 import net.minecraft.world.entity.Entity;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.Redirect;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(Entity.class)
 public abstract class EntityMixin {
+
+	@Unique private boolean lastWaterState;
+
+	@Shadow public abstract boolean isInWater();
+
+	@Inject(method = "tick", at = @At("TAIL"))
+	private void blep(CallbackInfo info) {
+		boolean inWater = this.isInWater();
+		if (inWater != this.lastWaterState) {
+			Swimmies.LOGGER.info("isInWater() = " + inWater);
+		}
+		this.lastWaterState = inWater;
+	}
 
 	@Redirect(method = {"updateSwimming", "isVisuallyCrawling", "canSpawnSprintParticle", "move"}, at = @At(value = "INVOKE", target = "Lnet/minecraft/world/entity/Entity;isInWater()Z"))
 	private boolean setInWater(Entity entity) {

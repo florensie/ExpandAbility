b8e36a90f17ea4a635fe76b2939059b74cf607f7
Florens Pauwels <mail@florens.be>
2021-03-31 00:19:39 +0200

    Skip surfaced check

--- README.md
+++ README.md
@@ -3,4 +3,3 @@ Minecraft mod that allows enabling swimming behaviour without being in water.
 
 ## To Do
 - Set fall height to 0 every tick and prevent fall damage
-- Fix not able to swim upwards without pressing space
---
+++ common/src/main/java/be/florens/swimmies/mixin/common/PlayerMixin.java
@@ -0,0 +1,16 @@
+package be.florens.swimmies.mixin.common;
+
+import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.level.material.FluidState;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Redirect;
+
+@Mixin(Player.class)
+public abstract class PlayerMixin {
+
+	@Redirect(method = "travel", allow = 1, at = @At(value = "INVOKE", target = "Lnet/minecraft/world/level/material/FluidState;isEmpty()Z"))
+	private boolean cancelSurfaceCheck(FluidState fluidState) {
+		return false;
+	}
+}

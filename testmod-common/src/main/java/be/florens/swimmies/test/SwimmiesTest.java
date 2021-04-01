104108317e66010622455c91612fcca5b9671654
Florens Pauwels <github@florens.be>
2021-04-01 18:42:50 +0200

    Platform specific testmods

--- testmod-common/src/main/java/be/florens/swimmies/test/SwimmiesTest.java
+++ testmod-common/src/main/java/be/florens/swimmies/test/SwimmiesTest.java
@@ -1,9 +1,5 @@
 package be.florens.swimmies.test;
 
-import be.florens.swimmies.api.PlayerSwimEvent;
-import net.minecraft.world.InteractionResult;
-import net.minecraft.world.item.Item;
-import net.minecraft.world.item.Items;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
@@ -13,12 +9,6 @@ public class SwimmiesTest {
 	public static final Logger LOGGER = LogManager.getLogger(MOD_ID);
 
 	public static void init() {
-		LOGGER.info("Swimmies testmod initialized");
-		PlayerSwimEvent.EVENT.register(event -> {
-			Item heldItem = event.getPlayer().getMainHandItem().getItem();
-			return heldItem == Items.DEBUG_STICK ? InteractionResult.SUCCESS
-					: heldItem == Items.BARRIER ? InteractionResult.FAIL
-					: InteractionResult.PASS;
-		});
+		LOGGER.info("Swimmies testmod initializing");
 	}
 }
--- testmod-forge/src/main/java/be/florens/swimmies/test/forge/SwimmiesTestForge.java
+++ testmod-forge/src/main/java/be/florens/swimmies/test/forge/SwimmiesTestForge.java
@@ -2,6 +2,10 @@ package be.florens.swimmies.test.forge;
 
 import be.florens.swimmies.api.forge.PlayerSwimEvent;
 import be.florens.swimmies.test.SwimmiesTest;
+import net.minecraft.world.item.Item;
+import net.minecraft.world.item.Items;
+import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.eventbus.api.Event;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
 
@@ -10,10 +14,15 @@ public class SwimmiesTestForge {
 
 	public SwimmiesTestForge() {
 		SwimmiesTest.init();
+		MinecraftForge.EVENT_BUS.register(this);
 	}
 
 	@SubscribeEvent
-	public static void onPlayerSwim(PlayerSwimEvent event) {
-
+	public void onPlayerSwim(PlayerSwimEvent event) {
+		Item heldItem = event.getPlayer().getMainHandItem().getItem();
+		Event.Result result = heldItem == Items.DEBUG_STICK ? Event.Result.ALLOW
+				: heldItem == Items.BARRIER ? Event.Result.DENY
+				: Event.Result.DEFAULT;
+		event.setResult(result);
 	}
 }

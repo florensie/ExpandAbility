da0273e587a510cadfaa2f258d88294245b0b276
Florens Pauwels <github@florens.be>
2022-03-05 16:52:10 +0100

    Mark methods as internal

--- common/src/main/java/be/florens/expandability/EventDispatcher.java
+++ common/src/main/java/be/florens/expandability/EventDispatcher.java
@@ -4,16 +4,19 @@ import dev.architectury.injectables.annotations.ExpectPlatform;
 import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.level.material.FluidState;
+import org.jetbrains.annotations.ApiStatus;
 
 public class EventDispatcher {
 
     @ExpectPlatform
+    @ApiStatus.Internal
     public static EventResult onPlayerSwim(Player player) {
-        return EventResult.PASS;
+        throw new IllegalStateException();
     }
 
     @ExpectPlatform
+    @ApiStatus.Internal
     public static boolean onLivingFluidCollision(LivingEntity entity, FluidState fluidState) {
-        return false;
+        throw new IllegalStateException();
     }
 }

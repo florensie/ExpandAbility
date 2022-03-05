6a998857f013cf6b1db4870a08cf8dd191f1cf43
Florens Pauwels <github@florens.be>
2022-03-05 16:52:03 +0100

    Use slf4j logger

--- common/src/main/java/be/florens/expandability/ExpandAbility.java
+++ common/src/main/java/be/florens/expandability/ExpandAbility.java
@@ -1,12 +1,12 @@
 package be.florens.expandability;
 
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
+import com.mojang.logging.LogUtils;
+import org.slf4j.Logger;
 
 public class ExpandAbility {
 
 	public static final String MOD_ID = "expandability";
-	public static final Logger LOGGER = LogManager.getLogger(MOD_ID);
+	public static final Logger LOGGER = LogUtils.getLogger();
 
 	public static void init() {
 		LOGGER.info("ExpandAbility here, who dis?");
